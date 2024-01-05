d5c0306d337e83cbea522f5f5971766b5b178eb3
lixin <lixin@enssi.cn>
2024-01-05 09:14:34 +0800

    修改build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,17 @@
 pluginManagement {
     repositories {
         google()
-        mavenLocal()
-        mavenCentral()
-        gradlePluginPortal()
+        jcenter()
         maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://maven.google.com") }
-        maven { url = uri("https://repo1.maven.org/maven2/") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public/") }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenLocal()
+        jcenter()
         mavenCentral()
-        gradlePluginPortal()
         maven { url = uri("https://www.jitpack.io") }
-        maven { url = uri("https://maven.google.com") }
-        maven { url = uri("https://repo1.maven.org/maven2/") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public/") }
     }
 }
 
